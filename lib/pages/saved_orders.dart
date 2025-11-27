2b9fb8efb90a12104f3783a27e8fdb44c1b96b70
bruno <bruno.rsilva2004@gmail.com>
2025-11-27 18:18:09 -0300

    implementa a tela de 'mais ferramentas' e a tela de 'pedidos salvos'.

--- lib/pages/saved_orders.dart
+++ lib/pages/saved_orders.dart
@@ -1,6 +1,69 @@
 import 'package:flutter/material.dart';
 
-class SavedOrders extends StatelessWidget {
+class SavedOrders extends StatefulWidget {
+  @override
+  State<SavedOrders> createState() => _SavedOrdersState();
+}
+
+class ButtonTypesGroup extends StatelessWidget {
+  const ButtonTypesGroup({super.key, this.onDiscard, this.onModify});
+
+  final VoidCallback? onDiscard;
+  final VoidCallback? onModify;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Expanded(
+          child: FilledButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(
+                const Color.fromARGB(255, 253, 49, 34),
+              ),
+              shape: MaterialStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(6),
+                ),
+              ),
+              
+            ),
+            onPressed: onDiscard ?? () {},
+            child: const Text('Descartar'),
+          ),
+        ),
+        const SizedBox(width: 8.0),
+        Expanded(
+          child: FilledButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(
+                const Color.fromARGB(255, 255, 147, 7),
+              ),
+              shape: MaterialStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(6),
+                ),
+              ),
+            ),
+            onPressed: onModify ?? () {},
+            child: const Text('Alterar'),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _SavedOrdersState extends State<SavedOrders> {
+  String? _selectedOrder;
+
+  void _selectedOrderApp(String? OrderId) {
+    setState(() {
+      _selectedOrder = OrderId;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +79,302 @@ class SavedOrders extends StatelessWidget {
           },
         ),
         title: Text('Pedidos Salvos'),
+        elevation: 03,
+        shadowColor: Colors.black87,
+        backgroundColor: Colors.white,
       ),
-      body: Center(child: Text('Olá Mundo!')),
+      body: Container(
+        color: Colors.grey[200],
+        child: ListView(
+          children: [
+            Container(
+              color: Colors.white,
+              child: ListTile(
+                onTap: () => _selectedOrderApp('1'),
+                tileColor: Colors.white,
+                leading: Radio<String>(
+                  value: '1',
+                  groupValue: _selectedOrder,
+                  onChanged: _selectedOrderApp,
+                ),
+                title: Text(
+                  '406 - W A PONTES ME',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Emissão: 17/11/2025',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                        Text(
+                          'Entrega: 29/11/2025',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Row(
+                      children: [
+                        Text(
+                          'Obs.: Adicionar 5% de desconto no total',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Desconto: 0,00',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Produtos: 56,90',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                        Text(
+                          'Flex: 2,39',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                        Text(
+                          'Total: 59,29',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Divider(height: 2, thickness: 1),
+
+            Container(
+              color: Colors.white,
+              child: ListTile(
+                onTap: () => _selectedOrderApp('2'),
+                tileColor: Colors.white,
+                leading: Radio<String>(
+                  value: '2',
+                  groupValue: _selectedOrder,
+                  onChanged: _selectedOrderApp,
+                ),
+                title: Text(
+                  '398 - FABIANO NEVES RODRIGUES',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Emissão: 09/11/2025',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                        Text(
+                          'Entrega: 12/11/2025',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Row(
+                      children: [
+                        Text(
+                          'Obs.: Cliente vai mandar o pix depois.',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Desconto: 3,90',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Produtos: 37,80',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                        Text(
+                          'Flex: -3,90',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                        Text(
+                          'Total: 33,90',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Divider(height: 2, thickness: 1),
+
+            Container(
+              color: Colors.white,
+              child: ListTile(
+                onTap: () => _selectedOrderApp('3'),
+                tileColor: Colors.white,
+                leading: Radio<String>(
+                  value: '3',
+                  groupValue: _selectedOrder,
+                  onChanged: _selectedOrderApp,
+                ),
+                title: Text(
+                  '406 - W A PONTES ME',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Emissão: 22/10/2025',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                        Text(
+                          'Entrega: 25/10/2025',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Row(
+                      children: [
+                        Text(
+                          'Obs.: ',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Desconto: 0,00',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Produtos: 85,20',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                        Text(
+                          'Flex: 7,97',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                        Text(
+                          'Total: 93,17',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Divider(height: 2, thickness: 1),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.all(12.0),
+        child: ButtonTypesGroup(),
+      ),
     );
   }
 }
--- lib/pages/tools_page.dart
+++ lib/pages/tools_page.dart
@@ -4,6 +4,7 @@ class ToolsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         leading: Builder(
           builder: (BuildContext context) {
@@ -16,8 +17,42 @@ class ToolsPage extends StatelessWidget {
           },
         ),
         title: Text('Outras Ferramentas'),
+        elevation: 03,
+        shadowColor: Colors.black87,
+        backgroundColor: Colors.white,
       ),
-      body: Center(child: Text('Olá Mundo!')),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            color: Colors.grey[800],
+            padding: EdgeInsets.all(10),
+            child: Text(
+              'CADASTROS',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 12,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          ListTile(
+            tileColor: Colors.white,
+            contentPadding: EdgeInsets.all(8),
+            title: Text(
+              'Gerar Catálogo dos Produtos',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+            subtitle: Text(
+              'Gera um catálogo em PDF dos Produtos Enviados para o APP.',
+              style: TextStyle(fontSize: 14, color: Colors.grey),
+            ),
+            trailing: Icon(Icons.chevron_right),
+            onTap: () {},
+          ),
+        ],
+      ),
     );
   }
 }
