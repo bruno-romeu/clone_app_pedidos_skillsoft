9adf9114dd1e20f9c1b127981c539db57c028293
bruno <bruno.rsilva2004@gmail.com>
2025-12-01 14:48:35 -0300

    implementa a tela de consulta de pedidos finalizados.

--- lib/pages/orders.dart
+++ lib/pages/orders.dart
@@ -1,9 +1,126 @@
 import 'package:flutter/material.dart';
 
+class TextFieldExampleApp extends StatelessWidget {
+  const TextFieldExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldExample();
+  }
+}
+
+class TextFieldExample extends StatefulWidget {
+  const TextFieldExample({super.key});
+
+  @override
+  State<TextFieldExample> createState() => _TextFieldExampleState();
+}
+
+class _TextFieldExampleState extends State<TextFieldExample> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(suffixIcon: Icon(Icons.search)),
+
+      controller: _controller,
+      onSubmitted: (String value) async {
+        await showDialog<void>(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Teste'),
+              content: Text('Teste'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class DatePickerApp extends StatelessWidget {
+  const DatePickerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const DatePickerExample();
+  }
+}
+
+class DatePickerExample extends StatefulWidget {
+  const DatePickerExample({super.key});
+
+  @override
+  State<DatePickerExample> createState() => _DatePickerExampleState();
+}
+
+class _DatePickerExampleState extends State<DatePickerExample> {
+  DateTime? selectedDate;
+
+  Future<void> _selectDate() async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime(2025, 12, 1),
+      firstDate: DateTime(2025),
+      lastDate: DateTime(2026),
+    );
+
+    setState(() {
+      selectedDate = pickedDate;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      spacing: 20,
+      children: <Widget>[
+        TextButton(
+          style: ButtonStyle(
+            foregroundColor: WidgetStateProperty.all(Colors.black),
+            textStyle: WidgetStateProperty.all(
+              TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          onPressed: _selectDate,
+          child: Text(
+            selectedDate != null
+                ? '${selectedDate!.day}/${selectedDate!.month}/${selectedDate!.year}'
+                : '${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}',
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class Orders extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
         leading: Builder(
           builder: (BuildContext context) {
@@ -16,8 +133,131 @@ class Orders extends StatelessWidget {
           },
         ),
         title: Text('Consulta de Pedidos'),
+        elevation: 03,
+        shadowColor: Colors.black87,
+        backgroundColor: Colors.white,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.replay_outlined), onPressed: () {}),
+        ],
       ),
-      body: Center(child: Text('Olá Mundo!')),
+      body: SingleChildScrollView(
+        child: Column(children: [InfoOrders(), FilterOrders()]),
+      ),
+    );
+  }
+}
+
+class InfoOrders extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+      ),
+      child: IntrinsicHeight(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Pedido de Venda:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                Text('Cancelados:', style: TextStyle(color: Colors.grey[700])),
+                Text(
+                  'Total Devolvido:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                Text(
+                  'Ag. Sincronização:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                Text(
+                  'Faturados (Vendas): ',
+                  style: TextStyle(color: Colors.grey[700]),
+                ),
+                Text(
+                  'Saldo Flex:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[700],
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  '0 Pedidos | Valor: 0,00',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text('0 Pedidos | Valor: 0,00'),
+                Text('0,00', style: TextStyle(fontWeight: FontWeight.bold)),
+                Text(
+                  '0 Pedidos',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text('0 Pedidos | Valor: 0,00'),
+                Text('0,00', style: TextStyle(fontWeight: FontWeight.bold)),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FilterOrders extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+      ),
+      child: IntrinsicHeight(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Column(children: [const DatePickerExample()]),
+                Column(children: [const DatePickerExample()]),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: TextFieldExample(),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
